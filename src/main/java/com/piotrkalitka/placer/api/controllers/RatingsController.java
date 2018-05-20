f64334ff1489ec6d4af3fa22aa3703b15e899155
Unknown <kitek1112@gmail.com>
2018-05-20 20:30:35 +0200

    Created empty RatingsController with Rating Model

---
+++ src/main/java/com/piotrkalitka/placer/api/controllers/RatingsController.java
@@ -0,0 +1,9 @@
+package com.piotrkalitka.placer.api.controllers;
+
+import com.piotrkalitka.placer.api.DataManager;
+
+public class RatingsController {
+
+    private DataManager dataManager = new DataManager();
+
+}
