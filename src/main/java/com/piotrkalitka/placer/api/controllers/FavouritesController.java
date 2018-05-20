d0edbdb5f394683ba40c6d44d65a67fcb0356b17
Unknown <kitek1112@gmail.com>
2018-05-20 20:21:28 +0200

    Created empty FavouritesController

---
+++ src/main/java/com/piotrkalitka/placer/api/controllers/FavouritesController.java
@@ -0,0 +1,9 @@
+package com.piotrkalitka.placer.api.controllers;
+
+import com.piotrkalitka.placer.api.DataManager;
+
+public class FavouritesController {
+
+    private DataManager dataManager = new DataManager();
+
+}
