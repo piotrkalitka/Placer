5d89fa117f6a7de3f9dbf7eb95984293389c46f0
Unknown <kitek1112@gmail.com>
2018-05-20 20:33:30 +0200

    Added missing controller annotations

--- src/main/java/com/piotrkalitka/placer/api/controllers/RatingsController.java
+++ src/main/java/com/piotrkalitka/placer/api/controllers/RatingsController.java
@@ -1,7 +1,13 @@
 package com.piotrkalitka.placer.api.controllers;
 
 import com.piotrkalitka.placer.api.DataManager;
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Component
+@RestController
+@RequestMapping(value = "/v1/places/{placeId}/ratings")
 public class RatingsController {
 
     private DataManager dataManager = new DataManager();
