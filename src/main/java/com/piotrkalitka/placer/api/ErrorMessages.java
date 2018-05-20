a8a22dc0d66bffd69529e8600a047886c56255cd
Unknown <kitek1112@gmail.com>
2018-05-20 19:53:24 +0200

    Created register endpoint with DataValidator, PasswordEncoder and ErrorMessages classes

---
+++ src/main/java/com/piotrkalitka/placer/api/ErrorMessages.java
@@ -0,0 +1,8 @@
+package com.piotrkalitka.placer.api;
+
+public final class ErrorMessages {
+    public static final String REGISTER_MISSING_DATA = "Missing data";
+    public static final String REGISTER_EMAIL_INVALID = "Invalid email";
+    public static final String REGISTER_PASSWORDS_NOT_MATCHES = "Passwords are not equals";
+    public static final String REGISTER_EMAIL_TAKEN = "Email address already taken";
+}
--- src/main/java/com/piotrkalitka/placer/api/controllers/AuthController.java
+++ src/main/java/com/piotrkalitka/placer/api/controllers/AuthController.java
@@ -1,7 +1,20 @@
 package com.piotrkalitka.placer.api.controllers;
 
+import com.piotrkalitka.placer.api.ApiError;
+import com.piotrkalitka.placer.api.DataManager;
+import com.piotrkalitka.placer.api.DataValidator;
+import com.piotrkalitka.placer.api.ErrorMessages;
+import com.piotrkalitka.placer.api.PasswordEncoder;
+import com.piotrkalitka.placer.api.apiModels.register.RegisterRequestModel;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @Component
@@ -9,6 +22,36 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "/v1/auth")
 public class AuthController {
 
+    private DataManager dataManager = new DataManager();
 
+    @RequestMapping(value = "/register", method = RequestMethod.POST)
+    @ResponseBody
+    public ResponseEntity<Object> register(@RequestBody RegisterRequestModel requestModel) {
+        String email = requestModel.getEmail();
+        String name = requestModel.getName();
+        String surname = requestModel.getSurname();
+        String password = requestModel.getPassword();
+        String passwordConfirmation = requestModel.getPasswordConfirmation();
+
+        if (DataValidator.isEmpty(email, name, surname, password, passwordConfirmation)) {
+            ApiError error = new ApiError(HttpStatus.BAD_REQUEST, ErrorMessages.REGISTER_MISSING_DATA);
+            return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
+        }
+        if (!DataValidator.isEmailValid(email)) {
+            ApiError error = new ApiError(HttpStatus.BAD_REQUEST, ErrorMessages.REGISTER_EMAIL_INVALID);
+            return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
+        }
+        if (!password.equals(passwordConfirmation)) {
+            ApiError error = new ApiError(HttpStatus.BAD_REQUEST, ErrorMessages.REGISTER_PASSWORDS_NOT_MATCHES);
+            return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
+        }
+        if (dataManager.isEmailRegistered(email)) {
+            ApiError error = new ApiError(HttpStatus.CONFLICT, ErrorMessages.REGISTER_EMAIL_TAKEN);
+            return new ResponseEntity<>(error, new HttpHeaders(), error.getStatus());
+        }
+
+        dataManager.addUser(email, name, surname, PasswordEncoder.encodePasswordSHA256(password));
+        return new ResponseEntity<>(HttpStatus.CREATED);
+    }
 
 }
