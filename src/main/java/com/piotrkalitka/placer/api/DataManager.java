6da1fb0a4755d6ca39d12cf5f65573277b98288f
Unknown <kitek1112@gmail.com>
2018-05-20 19:52:25 +0200

    added methods for getting users and checking if email address is in use

--- src/main/java/com/piotrkalitka/placer/api/DataManager.java
+++ src/main/java/com/piotrkalitka/placer/api/DataManager.java
@@ -3,14 +3,34 @@ package com.piotrkalitka.placer.api;
 import com.piotrkalitka.placer.api.dbModels.User;
 
 import org.hibernate.Session;
+import org.hibernate.query.Query;
+import org.springframework.lang.Nullable;
 
 import javax.persistence.EntityManager;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 public class DataManager {
 
     private EntityManager entityManager = Persistence.createEntityManagerFactory("dbManager").createEntityManager();
     private Session session = (Session) entityManager.getDelegate();
+
+    @Nullable
+    public User getUser(String email) {
+        CriteriaBuilder builder = session.getCriteriaBuilder();
+        CriteriaQuery<User> query = builder.createQuery(User.class);
+        Root<User> root = query.from(User.class);
+        query.where(builder.equal(root.get("email"), email));
+        query.select(root);
+        Query<User> q = session.createQuery(query);
+        return q.uniqueResult();
+    }
+
+    public boolean isEmailRegistered(String email) {
+        return getUser(email) != null;
+    }
 
     public void addUser(String email, String name, String surname, String password) {
         User user = new User(email, name, surname, password);
